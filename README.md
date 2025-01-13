f63843ef2dcc6b6b26213f077af1e2c8ddf4b726
kiruse <26940742+Kiruse@users.noreply.github.com>
2025-01-13 08:29:17 +0100

    initial project

---
+++ README.md
@@ -0,0 +1,68 @@
+# convex-signals-client
+An integration of [Convex](https://www.convex.dev/) with [Preact Signals](https://preactjs.com/guide/v10/signals).
+
+## Installation
+Install using your favorite package manager's equivalent of:
+
+```bash
+$ npm install convex @preact/signals convex-signals-client
+```
+
+## Usage
+```typescript
+import { ConvexSignalsClient } from "convex-signals-client";
+import { api } from "./convex/_generated/api";
+
+const client = new ConvexSignalsClient(process.env.CONVEX_URL!);
+const signal = client.querySignal(api.example.exampleQuery);
+
+signal.subscribe((value) => {
+  console.log(value);
+});
+
+const computed = client.queryComputed(api.example.exampleQuery, () => {
+  // [args, options], as if you were calling `query`
+  return [{ values: [1, 2, 3] }];
+});
+
+await client.query(api.example.exampleQuery, { values: [1, 2, 3] });
+await client.mutation(api.example.exampleMutation, { values: [1, 2, 3] });
+await client.action(api.example.exampleAction, { values: [1, 2, 3] });
+```
+
+*Convex Signals Client* creates a wrapper around both a Convex query and a Preact signal called `ConvexSignal`. This special signal exposes some additional properties:
+
+```typescript
+export interface ConvexSignal<Query extends FunctionReference<"query">> {
+  get value(): FunctionReturnType<Query>;
+  get isLoaded(): boolean;
+  subscribe(fn: (value: FunctionReturnType<Query> | undefined) => void): (() => void);
+  refresh(): void;
+  destroy(): void;
+}
+```
+
+- `value`: The current locally cached value of the query. Integrated with Convex's `onTransition` to update its value automatically.
+- `isLoaded`: Reactive flag indicating if the query has been loaded from the server at least once. Once true, this flag should never revert back to false. Using this flag will, like `value`, hook into *Preact Signals'* reactivity system.
+- `subscribe`: Subscribe to the signal.
+- `refresh`: Manually refresh the signal value from the server.
+- `destroy`: Manually unsubscribe the signal from the Convex query. The signal will no longer automatically update, but you may still be able to `refresh` it.
+
+## Caveats
+Due to lack of direct resource management control, there are some edge cases to avoid bugs. This library is built under the assumption that you will generally use one of two patterns:
+
+- Global signals: Keeping signals around indefinitely for the lifetime of your application. In this case, the signal will live forever.
+- Subscribers: Using the `subscribe` method to keep your application in sync with the server. The final unsubscribe method called will also destroy the signal.
+
+For other patterns, you may need to manually manage the signal's lifecycle by keeping a subscription active and/or calling `destroy` on it. I'll gladly accept PRs to improve the lifecycle management of the signals or to hook into garbage collection.
+
+## License
+The MIT License (MIT)
+Copyright © 2025 Kiruse
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ src/client.ts
@@ -0,0 +1,152 @@
+import { computed, signal } from '@preact/signals';
+import { BaseConvexClient, type MutationOptions, type QueryToken } from 'convex/browser';
+import type { ArgsAndOptions, FunctionReturnType } from 'convex/server';
+import { getFunctionName } from 'convex/server';
+import type { FunctionReference } from 'convex/server';
+
+const IsLoaded = Symbol('IsLoaded');
+
+export interface QuerySignalOptions {}
+
+export interface QueryOptions {
+  /** Maximum time to wait before rejecting the promise. Defaults to 10 seconds. */
+  timeout?: number;
+}
+
+/** A `ConvexSignal` is a readonly signal integrating with Convex reactive queries. */
+export interface ConvexSignal<Query extends FunctionReference<"query">> {
+  get value(): FunctionReturnType<Query>;
+  get isLoaded(): boolean;
+  subscribe(fn: (value: FunctionReturnType<Query> | undefined) => void): (() => void);
+  /** Refreshes the signal value from the underlying state. Called automatically by the client. You only need `value` or `subscribe` to use the signal.zs */
+  refresh(): void;
+  /** Unsubscribe from the underlying Convex query. The signal will no longer update. */
+  destroy(): void;
+}
+
+export class ConvexSignalsClient {
+  #client: BaseConvexClient;
+  #signals = new Map<QueryToken, ConvexSignal<any>>();
+
+  constructor(baseUrl: string) {
+    this.#client = new BaseConvexClient(
+      baseUrl,
+      this.#onTransition,
+      {},
+    );
+  }
+
+  #onTransition = (updatedQueries: QueryToken[]) => {
+    for (const queryToken of updatedQueries) {
+      const signal = this.#signals.get(queryToken);
+      if (signal) {
+        (signal as any)[IsLoaded] = true;
+        signal.refresh();
+      }
+    }
+  };
+
+  action<Action extends FunctionReference<"action">>(
+    action: Action,
+    ...argsAndOptions: ArgsAndOptions<Action, {}>
+  ): Promise<FunctionReturnType<Action>> {
+    return this.#client.action(getFunctionName(action), ...argsAndOptions);
+  }
+
+  /** Create a reactive signal from a static Convex query. */
+  querySignal<Query extends FunctionReference<"query">>(
+    query: Query,
+    ...argsAndOptions: ArgsAndOptions<Query, QuerySignalOptions>
+  ): ConvexSignal<Query> {
+    const name = getFunctionName(query);
+    const [args, options] = argsAndOptions;
+    const { queryToken, unsubscribe } = this.#client.subscribe(name, args, options);
+
+    if (this.#signals.has(queryToken)) {
+      unsubscribe();
+      return this.#signals.get(queryToken)!;
+    }
+
+    const sig = signal<FunctionReturnType<Query>>();
+    let counter = 0;
+    const result = {
+      [IsLoaded]: false,
+      get value() {
+        return sig.value;
+      },
+      get isLoaded() {
+        // force hooking into effects & computed signals so that `isLoaded` can be used as condition
+        // for `value`.
+        sig.value;
+        return this[IsLoaded];
+      },
+      subscribe: (fn: (value: FunctionReturnType<Query> | undefined) => void) => {
+        counter++;
+        const unsub = sig.subscribe(fn);
+        return () => {
+          unsub();
+          if (--counter === 0) {
+            unsubscribe();
+            this.#signals.delete(queryToken);
+          }
+        };
+      },
+      refresh: () => {
+        sig.value = this.#client.localQueryResult(name, args);
+      },
+      destroy: () => {
+        this.#signals.delete(queryToken);
+        unsubscribe();
+      },
+    };
+    this.#signals.set(queryToken, result);
+    return result;
+  }
+
+  /** Create a reactive signal from a Convex query responding to signal changes. */
+  queryComputed<Query extends FunctionReference<"query">>(
+    query: Query,
+    fnArgsAndOptions: () => ArgsAndOptions<Query, QueryOptions>
+  ): ConvexSignal<Query> {
+    const sig = computed(() => {
+      return this.querySignal(query, ...fnArgsAndOptions());
+    });
+    return {
+      get value() {
+        return sig.value.value;
+      },
+      get isLoaded() {
+        return sig.value.isLoaded;
+      },
+      refresh: () => sig.value.refresh(),
+      subscribe: (fn: (value: FunctionReturnType<Query> | undefined) => void) => sig.value.subscribe(fn),
+      destroy: () => sig.value.destroy(),
+    }
+  }
+
+  query<Query extends FunctionReference<"query">>(
+    query: Query,
+    ...argsAndOptions: ArgsAndOptions<Query, QueryOptions>
+  ): Promise<FunctionReturnType<Query>> {
+    const [args, options] = argsAndOptions;
+    const signal = this.querySignal(query, args, options);
+    return new Promise<FunctionReturnType<Query>>((resolve, reject) => {
+      const timer = setTimeout(() => reject(new Error('Query timed out')), options?.timeout ?? 10000);
+      const unsub = signal.subscribe((value) => {
+        if (!signal.isLoaded) return;
+        clearTimeout(timer);
+        resolve(value);
+        unsub();
+      });
+    })
+  }
+
+  mutation<Mutation extends FunctionReference<"mutation">>(
+    mutation: Mutation,
+    ...argsAndOptions: ArgsAndOptions<Mutation, MutationOptions>
+  ): Promise<FunctionReturnType<Mutation>> {
+    const name = getFunctionName(mutation);
+    const [args, options] = argsAndOptions;
+    return this.#client.mutation(name, args, options);
+  }
+}
